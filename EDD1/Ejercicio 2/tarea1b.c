d8de56997512914a71c83e1a0c6047afa87deeb4
WisdomSeller <64997816+WisdomSeller@users.noreply.github.com>
2020-05-19 19:18:59 -0400

    Add files via upload

--- EDD1/Ejercicio 2/tarea1b.c
+++ EDD1/Ejercicio 2/tarea1b.c
@@ -3,12 +3,15 @@
 #include <math.h>
 #include <string.h>
 
-clienteBanco* leerTrans(char* transacciones, struct* finalCuentas, int totalClientes){
+clienteBanco* leerTrans(char* transacciones, clienteBanco* finalCuentas, int totalClientes){
     FILE *fp;
     int tam,c1,c2,dinero;
     char temp,signo;
 
     fp = fopen(transacciones, "r+");
+    if (fp==NULL){
+        printf("Error al abrir archivo")
+    }
     while (fread(&temp, sizeof(char),1,fp)!= EOF){
         if (temp=="+"){
             fscanf(fp, "%c %d %d",signo, c1, dinero);
@@ -46,15 +49,21 @@ int contarlineas(char *file){
     FILE *fp;
     int cont=0;
     fp=fopen(file,"rb");
+    if (fp==NULL){
+        printf("Error al abrir archivo")
+    }
     while (fread(&cliente,sizeof(clienteBanco),1,fp)!=0){
         cont++;
   }
     fclose(fp);
     return cont;
 }
-clienteBanco *Cuentas(char *fname){
-    int n=contarlineas(fname),i;
+clienteBanco *Cuentas(char *fname, int n){
+    int i=0;
     FILE *fp=fopen(fname,"rb");
+    if (fp==NULL){
+        printf("Error al abrir archivo")
+    }
     clienteBanco clientes=(clienteBanco)malloc(sizeof(clienteBanco)*n);
     while (1){
         clientes[i]=cliente;
@@ -69,8 +78,15 @@ clienteBanco *Cuentas(char *fname){
 }
 
 void actualizarSaldos(char *clientes, char *transacciones){
-    
-
+    int n =contarlineas(clientes);
+    clienteBanco *cuentasClientes= Cuentas(clientes);
+    cuentasClientes=leerTrans(transacciones,cuentasClientes,n);
+    FILE *fp;
+    fp= fopen(clientes,"w");
+    if (fp==NULL){
+        printf("Error al abrir archivo")
+    }
+    fwrite(cuentasClientes, clienteBanco,n, fp);
 }
 
 typedef struct {
@@ -81,5 +97,9 @@ typedef struct {
 } clienteBanco; 
 
 int main(char* clientes, char* transacciones){
-
+    if (clientes == NULL || transacciones== NULL){
+        printf("Inserte los nombres correctos para los archivos")
+    }
+    actualizarSaldos(char* clientes, char* transacciones);
+    return 0;
 }
