e1333fc7f85d839a6f03d208b666579f06ee1001
bluelf45 <52522150+bluelf45@users.noreply.github.com>
2020-06-25 19:26:11 -0400

    Update TDAlista.c

--- Tarea2/TDAlista.c
+++ Tarea2/TDAlista.c
@@ -3,7 +3,17 @@
 #include "TDAlista.h"
 //lista1 memoria disponible
 //lista2 memoria usada
-
+tNodo* centinela;
+int move_next(tLista* lista){
+    if(lista->curr->next!=NULL){
+        lista->curr=lista->curr->next;
+        lista->pos++;
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
 void initList(tLista* lista,int tam){
     lista->curr=(tNodo *)malloc(sizeof(tNodo));
     lista->head=lista->curr;
@@ -12,17 +22,42 @@ void initList(tLista* lista,int tam){
     lista->listSize=tam;
     lista->pos=0;
 }
-int insert(tLista* lista, int item1, int item2){
-    tNodo* aux=lista->curr->next;
-    lista->curr->next=malloc(sizeof(tNodo));
-    lista->curr->next->data1=item1;
-    lista->curr->next->data2=item2;
-    lista->curr->next->next=aux;
+int remove(tLista* lista,int data1){
+    search(lista,data1);
     if(lista->curr==lista->tail){
-        lista->tail=lista->curr->next;
+        anterior(lista);
+        free(lista->curr->next);
+        lista->curr->next=NULL;
+        lista->tail=lista->curr;
+        lista->listSize-=1;
     }
-    lista->listSize++;
-    return lista->pos;
+    else if(lista->curr==lista->head){
+        lista->
+    }
+}
+void insert(tLista* lista, int data1,int data2,tLista* lista2){
+    while(lista->curr->dato1<data1){
+        move_next(lista);
+    }
+    if(lista->curr->dato1==data2+1){
+        lista->curr->dato1=data1;
+    }
+    else if(lista->curr->dato2==data1-1){
+        lista->curr->dato2=data2;
+    }
+    remove(lista,data1);
+}
+
+
+void anterior(tLista* lista){
+    tNodo* aux=lista->curr;
+    move_to_head(lista);
+    while(1){
+        if(lista->curr->next==aux){
+            break;
+        }
+        move_next(lista);
+    }
 }
 void move_to_head(tLista* lista){
     lista->curr=lista->head;
@@ -32,45 +67,60 @@ void move_to_tail(tLista* lista){
     lista->curr=lista->tail;
     lista->pos=lista->listSize-1;
 }
-int limpieza(tNodo* head){
-    tNodo aux=lista->head->next;
-    free(lista->head);
+int limpieza(tNodo* nodo){
+    tNodo* aux=nodo->next;
+    free(nodo);
     if(aux==NULL){
         return 1;
     }
     limpieza(aux);
     return 0;
 }
-void clear(tLista* lista){
-    return limpieza(lista->head);
-}
-int espacio(tLista* lista, int tam){
-    return 1;
-}
-int search(tNodo* nodo, int dato){
-    if(lista->head->dato1==dato){
-        lista->curr=lista->head;
-        return 1;
-    }
-    tNodo* aux=lista->head->next;
-    else if(aux->dato1==dato){
-        lista->curr->aux;
-        return 1;
-    }
-    search(aux,dato);
+int clear(tLista* lista){
+    limpieza(lista->head);
     return 0;
 }
-
-int liberar(tLista* lista, int inicio){
-    if(search(lista->head, inicio)==0){
-        printf("no se encontro la Memoria\n");
-        return 0;
+int search(tLista* lista, int dato){
+    move_to_head(lista);
+    while(lista->curr!=NULL){
+        if(lista->curr->data1==dato){
+            return 1;
+        }
+        else{
+            centinela=lista->curr;
+            lista->pos++;
+            lista->curr=lista->curr->next;
+        }
     }
+    move_to_head(lista);
+    return 0;
+}
+//lista->a la que se eliminar
+//lista2->a la que se le va a agregar
+//en proceso
+/*int free2(tlista* lista, int inicio,tista* lista2){
+    if(search(lista, inicio)==0){
+        return 0;
+    };
+    //quitar cola
+    verificar_espacio()
     if (lista->curr->next==NULL){
-
+        free(lista->curr);
+        lista->tail=centinela;
+        lista->tail=NULL
+    }
+    //quitar cabeza
+    else if(lista->pos==0){
+        lista->head=lista->curr->next;
+        free(lista->curr);
+    }
+    //quitar entremedio
+    else{
+        centinela->next=lista->curr->next;
+        free(lista->curr);
     }
 }
-
+*/
 int main(){
     return 0;
 }
