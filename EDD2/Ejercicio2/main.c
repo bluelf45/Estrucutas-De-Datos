7b06564d484af2a73e2c56cd1aeac0bf2594f7c1
bluelf45 <52522150+bluelf45@users.noreply.github.com>
2020-07-04 16:49:09 -0400

    Update main.c

--- EDD2/Ejercicio2/main.c
+++ EDD2/Ejercicio2/main.c
@@ -12,23 +12,16 @@ int main(){
         return 1;
     }
     fscanf(fp,"%d", &u);
-    printf("u=%d \n",u);
     arbolBin* arbol=(arbolBin*)malloc(sizeof(arbolBin));
     initArbol(arbol);
-    int suce;
     while(fscanf(fp,"%s %d", decidor, &numero)!=EOF){
         if(strcmp(decidor,"SUCESOR")==0){
-            suce=Sucesor(arbol, numero, u);
-            if(suce!=-1){
-                printf("%d\n",suce);
-            }
+            Sucesor(arbol, numero, u);
         }
         if(strcmp(decidor,"BORRAR")==0){
-            printf("borrar\n");
             Borrar(arbol, numero);
         }
         else if(strcmp(decidor,"PREORDEN")==0){
-            printf("preorden: ");
             preorden(arbol);
         }
         else if(strcmp(decidor,"INSERTAR")==0){
