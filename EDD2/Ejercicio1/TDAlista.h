cbe38c66c3e2069165f1528ba36eedbbbad929ec
bluelf45 <52522150+bluelf45@users.noreply.github.com>
2020-07-04 16:47:13 -0400

    Update TDAlista.h

--- EDD2/Ejercicio1/TDAlista.h
+++ EDD2/Ejercicio1/TDAlista.h
@@ -1,5 +1,6 @@
 #ifndef TDA_LISTA_H
 #define TDA_LISTA_H
+#include <stdio.h>
 typedef struct nodo {
     int data1;//inicio
     int data2;//final
@@ -17,7 +18,7 @@ typedef struct {
 //retorna 1 si se hizo correctamente y 0 si no se logro
 int move_next(tLista* lista);
 
-int contarBytes (tLista* lista);
+int  contarBytes (tLista* lista);
 
 void initList(tLista* lista);
 
@@ -25,19 +26,25 @@ void initNodo(tNodo* nodo, int dato1, int dato2);
 
 void anterior(tLista* lista);
 
+void push(tLista* lista,int data1,int data2);
+
 int insert_nodo(tLista*lista,tNodo* nodo);
 
 tNodo* remove2(tLista* lista,int data1);
 
+void printlista(tLista* lista);
+
 void move_to_head(tLista* lista);
+
+void move_to_tail(tLista* lista);
 
 void clear(tLista* lista);//eliminar la lista
 
 int search(tLista* lista, int dato);
 
-void free2(tLista* listaD, tLista* listaU, int byte);
+void free2(tLista* listaD, tLista* listaU, int byte,  FILE* fp);
 
-int malloc2(tLista* listaD, tLista* listaU, int bytesize);
+int malloc2(tLista* listaD, tLista* listaU, int bytesize, FILE* fp);
 
 int tamLista(tLista* lista);
 
