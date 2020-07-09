cddf1c752329dd5af394f9c78debe8e40d3a1c2d
bluelf45 <52522150+bluelf45@users.noreply.github.com>
2020-07-09 17:38:38 -0400

    Update TDArbol.h

--- EDD2/Ejercicio2/TDArbol.h
+++ EDD2/Ejercicio2/TDArbol.h
@@ -15,17 +15,31 @@ typedef struct {
 } arbolBin;
 
 void initArbol(arbolBin* arbol);
+
 void initNodo(tNodo* nodo, int item);
+
 void clearHelp(tNodo* nodo);
+
 void clear(arbolBin* arbol);
+
 tNodo* encontrarMax(tNodo* nodo);
+
 tNodo* borrarHelp(tNodo* nodo, int item);
+
 int Borrar(arbolBin* arbol, int item);
+
 void Sucesor(arbolBin* arbol, int item, int u);
+
 void sucesorHelp(tNodo* nodo, int item);
+
 void checkderecha(tNodo* nodo, int item, int u);
+
 int insertHelp(tNodo* nodo, int item);
+
 void insertar(arbolBin* arbol, int item);
+
 void printPreNodo(tNodo* nodo);
+
 void preorden(arbolBin* arbol);
+
 #endif
